b2ee9bad7c4053e4801801329cb7d1735f953cb9
wassim93 <wassim.benhassen@esprit.tn>
2022-01-09 22:14:52 +0000

    fixing style issue & adding router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>im checkout page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
