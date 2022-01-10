6e239634b1fbc499a4c537eec198521dea6f9b3e
wassim93 <b.wassim1993@hotmail.fr>
2022-01-10 17:33:25 +0100

    adding firebase confgi

--- src/config/firebaseServices.js
+++ src/config/firebaseServices.js
@@ -2,3 +2,6 @@ import firebase from "firebase";
 import { firebaseConfig } from "./firebase";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { db, auth };
