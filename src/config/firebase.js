294f7972cb3467a99a54b7b07902eda95c0ecab5
wassim93 <wassim.benhassen@esprit.tn>
2022-01-10 19:50:34 +0000

    update

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// change those config with your firebase config project
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "xxxxxxxx",
+  projectId: "xxxxxx",
+  storageBucket: "xxxxxx",
+  messagingSenderId: "xxxxxx",
+  appId: "xxxxx",
+  measurementId: "xxxxxxxxx",
+};
